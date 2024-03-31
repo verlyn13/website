0ae6dc880b83eb77569b9490afc34c65ffd03a56
Jeffrey Johnson <jeffreyverlynjohnson@gmail.com>
2024-03-30 19:05:46 -0800

    Local changes

--- checkout.bat
+++ checkout.bat
@@ -1,4 +1,5 @@
 @echo off
+
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 echo Starting checkout process...
@@ -7,8 +8,8 @@ REM Render the Quarto project
 echo Rendering Quarto project...
 quarto render
 IF !ERRORLEVEL! NEQ 0 (
-    echo Failed to render Quarto project. Exiting...
-    goto :error
+  echo Failed to render Quarto project. Exiting...
+  goto :error
 )
 
 REM Add and commit to main
@@ -18,16 +19,16 @@ git add .
 git commit -m "Local changes"
 git push origin main
 IF !ERRORLEVEL! NEQ 0 (
-    echo Failed to push changes to main. Exiting...
-    goto :error
+  echo Failed to push changes to main. Exiting...
+  goto :error
 )
 
 REM Switch to gh-pages branch
 echo Switching to gh-pages branch...
 git checkout gh-pages
 IF !ERRORLEVEL! NEQ 0 (
-    echo Failed to switch to gh-pages branch. Exiting...
-    goto :error
+  echo Failed to switch to gh-pages branch. Exiting...
+  goto :error
 )
 
 REM Clear gh-pages branch
@@ -35,16 +36,16 @@ echo Clearing old content from gh-pages...
 git rm -rf ./*
 git clean -fdx
 IF !ERRORLEVEL! NEQ 0 (
-    echo Failed to clear old content. Exiting...
-    goto :error
+  echo Failed to clear old content. Exiting...
+  goto :error
 )
 
 REM Copy new content from _site to gh-pages
 echo Copying new content from _site to gh-pages...
-xcopy /E /I /Y "_site\*" ".\"
+xcopy /E /I /Y "_site\*" "%cd%"
 IF !ERRORLEVEL! NEQ 0 (
-    echo Failed to copy new content. Exiting...
-    goto :error
+  echo Failed to copy new content. Exiting...
+  goto :error
 )
 
 REM Commit changes to gh-pages
@@ -53,8 +54,8 @@ git add --all
 git commit -m "Update website"
 git push origin gh-pages
 IF !ERRORLEVEL! NEQ 0 (
-    echo Failed to push to gh-pages. Exiting...
-    goto :error
+  echo Failed to push to gh-pages. Exiting...
+  goto :error
 )
 
 echo Checkout process completed.
